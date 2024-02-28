7a529de48022b1f5c1bb53cd5b970a600033132b
eroinne <lambeybaptiste@gmail.com>
2024-02-28 14:02:32 +0100

    Initial Commit

---
+++ Sae_Cocagne/Sae_CocagneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sae_CocagneApp.swift
+//  Sae_Cocagne
+//
+//  Created by Ero on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Sae_CocagneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
