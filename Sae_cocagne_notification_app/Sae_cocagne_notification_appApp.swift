60b6c742016aaedaaa6725a99c055e981550a9bd
eroinne <lambeybaptiste@gmail.com>
2024-03-13 16:17:07 +0100

    Initial Commit

---
+++ Sae_cocagne_notification_app/Sae_cocagne_notification_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sae_cocagne_notification_appApp.swift
+//  Sae_cocagne_notification_app
+//
+//  Created by Ero on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Sae_cocagne_notification_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
